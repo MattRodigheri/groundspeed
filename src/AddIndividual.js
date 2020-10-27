01176dac12f6f292b4757fe3d5cbaac1d3588eaa
MattRodigheri <mrodigheri@gmail.com>
2020-10-27 12:12:10 -0700

    addIndividual function passed to child comp

--- src/AddIndividual.js
+++ src/AddIndividual.js
@@ -1,7 +1,18 @@
-function AddIndividual() {
+
+
+function AddIndividual(props) {
     return (
       <div>
-          Add Individual:
+        <h1>Add Individual:</h1>
+        <h3>Gender:</h3>
+        <input type="text"/>
+        <h3>Date of Birth:</h3>
+        <input type="text"/>
+        <h3>Social Security Number:</h3>
+        <input type="text"/>
+        <p>clicking 'Add' will generate a unique user ID</p>
+        <button onClick={props.addIndividual}>Add</button>
+        <div className="idOutput"></div>
       </div>
     );
   }
--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
+import React from 'react'
 import './App.css';
 import AddIndividual from './AddIndividual.js'
 import AddEvent from "./AddEvent.js"
 import ListAllIndividuals from "./ListAllIndividuals.js"
 import ListAllEventsByIndividual from "./ListAllEventsByIndividual"
 
-function App() {
-  return (
-    <div className="App">
-      <AddIndividual/>
-      {/* <AddEvent/>
-      <ListAllIndividuals/>
-      <ListAllEventsByIndividual/> */}
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state={
+
+    }
+
+    this.addIndividual = this.addIndividual.bind(this)
+  }
+
+  addIndividual = () => {
+    console.log('test')
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <AddIndividual addIndividual={this.addIndividual}/>
+        {/* <AddEvent/>
+        <ListAllIndividuals/>
+        <ListAllEventsByIndividual/> */}
+      </div>
+    );
+  }
 }
 
 export default App;
